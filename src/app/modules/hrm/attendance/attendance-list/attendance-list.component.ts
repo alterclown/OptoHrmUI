439934305d742019b4717ac41182d5cd8bcbf6e2
alterclown <rahi2721@gmail.com>
2021-04-27 18:17:03 +0600

    pagingRunning

--- src/app/modules/hrm/attendance/attendance-list/attendance-list.component.ts
+++ src/app/modules/hrm/attendance/attendance-list/attendance-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 import { AttendanceService } from '../services/attendance.service';
 
 @Component({
@@ -11,17 +12,18 @@ export class AttendanceListComponent implements OnInit {
   pageNumber = 1;
   page: any;
   pageDataLimit = 10;
-  constructor(private attendanceService: AttendanceService,) {
+  constructor(private attendanceService: AttendanceService, private spinner: NgxSpinnerService) {
   }
 
   ngOnInit(): void {
     this.getData();
   }
   getData() {
+    this.spinner.show();
     this.attendanceService.getAttendance(this.pageNumber, this.pageDataLimit).subscribe(data => {
       this._attendanceList = data;
-      //this.firstName = JSON.stringify( data );
     });
+    this.spinner.hide();
   }
   deleteAttendance(attendanceId: number) {
     this.attendanceService.deleteAttendance(attendanceId).subscribe(() => {
@@ -30,16 +32,24 @@ export class AttendanceListComponent implements OnInit {
     this._attendanceList.splice(0, 1);
   }
   handleNextPage(e: any) {
-    this.page = this.pageNumber++;
-    this.attendanceService.getAttendance(this.page, this.pageDataLimit).subscribe(data => {
-      this._attendanceList = data;
-    });
+    if (this.pageNumber > 0) {
+      this.page = this.pageNumber++;
+      this.spinner.show();
+      this.attendanceService.getAttendance(this.page, this.pageDataLimit).subscribe(data => {
+        this._attendanceList = data;
+      });
+      this.spinner.hide();
+    }
   }
 
   handlePreviousPage(e: any) {
-    this.page = this.pageNumber--;
-    this.attendanceService.getAttendance(this.page, this.pageDataLimit).subscribe(data => {
-      this._attendanceList = data;
-    });
+    if (this.pageNumber > 1 && this.pageNumber != 0) {
+      this.page = this.pageNumber--;
+      this.spinner.show();
+      this.attendanceService.getAttendance(this.page, this.pageDataLimit).subscribe(data => {
+        this._attendanceList = data;
+      });
+      this.spinner.hide();
+    }
   }
 }
